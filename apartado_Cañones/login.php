b32204f548ec2912801b50e38a5fb93c9fb9d91e
LFernandoTorres <46614380+LFernandoTorres@users.noreply.github.com>
2019-09-19 20:11:01 -0500

    Login 001
    Login Plano

--- apartado_Cañones/login.php
+++ apartado_Cañones/login.php
@@ -8,7 +8,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>SB Admin - Login</title>
+  <title>Apartado Cañones - Login</title>
 
   <!-- Custom fonts for this template-->
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
